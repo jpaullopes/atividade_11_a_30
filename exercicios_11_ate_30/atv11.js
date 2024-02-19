5d3e547b4cf7162f4b01c8d7ff90ae21bc5d1ff7
jão <159722770+jpaullopes@users.noreply.github.com>
2024-02-19 20:54:56 -0300

    atv11.js

--- exercicios_11_ate_30/atv11.js
+++ exercicios_11_ate_30/atv11.js
@@ -3,13 +3,10 @@ import { question } from "readline-sync"
 //entrada
 const numero = Number(question('Digite o número que você quer inverter[3 dígitos]: '))
 //processamento
-/*converter em uma string -> separar cada elemento e colocar em uma lista 
--> inverter as ordens dos elementos dessa lista ->
--> concatenar essa lista em uma string denovo -> tranformar em um número denovo */
-var numero_invertido = numero.toString()
-numero_invertido = numero_invertido.split('')
-numero_invertido.reverse()
-numero_invertido = numero_invertido.join('')
-numero_invertido = Number(numero_invertido)
+const centena = Number.parseInt(numero / 100)
+const resto = numero % 100
+const dezena = Number.parseInt(resto / 10)
+const unidade = resto % 10
+const numero_invertido = unidade * 100 + dezena * 10 + centena
 //saída
 console.log(`O inverso do número ${numero} é o número ${numero_invertido}. `)
